44a163616fba057ccdd283ead10304b0b22c932f
Admin <b50404201@gmail.com>
2025-12-27 21:51:49 +0900

    CustomUserDetailsService 수정

--- src/main/java/com/example/loginbe/service/CustomUserDetailsService.java
+++ src/main/java/com/example/loginbe/service/CustomUserDetailsService.java
@@ -15,8 +15,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String phone) throws UsernameNotFoundException {
-        User user = userRepository.findByPhone(phone)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
         return new CustomUserDetails(user);
     }
